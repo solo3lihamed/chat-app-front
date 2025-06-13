bed4904b42e18d1f215eb1411029c1cfaba8b8a3
solo3lihamed <fps60y@gmail.com>
2025-06-13 23:56:45 +0300

    1:45

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,39 @@ function App() {
 
   return (
     <>
-      <div>hamed</div>
+      <h1 class="text-3xl font-bold underline text-red-300">
+    Hello world!
+  </h1>
+
+
+  <div className="avatar">
+  <div className="w-24 rounded">
+    <img src="https://img.daisyui.com/images/profile/demo/batperson@192.webp" />
+  </div>
+</div>
+
+<div className="avatar-group -space-x-6">
+  <div className="avatar">
+    <div className="w-12">
+      <img src="https://img.daisyui.com/images/profile/demo/batperson@192.webp" />
+    </div>
+  </div>
+  <div className="avatar">
+    <div className="w-12">
+      <img src="https://img.daisyui.com/images/profile/demo/spiderperson@192.webp" />
+    </div>
+  </div>
+  <div className="avatar">
+    <div className="w-12">
+      <img src="https://img.daisyui.com/images/profile/demo/averagebulk@192.webp" />
+    </div>
+  </div>
+  <div className="avatar">
+    <div className="w-12">
+      <img src="https://img.daisyui.com/images/profile/demo/wonderperson@192.webp" />
+    </div>
+  </div>
+</div>
         
     </>
   )
